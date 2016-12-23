fe972074757d95d0dcf49c4f713188bbf15f9ab0
Nanciee <nancyduyi@gmail.com>
2016-12-23 02:37:24 -0500

    Add tech stack and percentage

--- client/app/about/about.js
+++ client/app/about/about.js
@@ -1,5 +1,5 @@
 angular.module('personal.about', [])
 .controller('AboutController', function() {
 	var AboutVm = this;
-	AboutVm.skills = ["JavaScript", "TypeScript", "HTML/CSS", "Sass/LESS", "jQeury", "Angular", "Angular 2", "React", "React Native", "Backbone", "Node/Express", "MongoDB", "MySQL", "Git", "Grunt/Gulp", "Mocha/Chai"]
+	AboutVm.skills = [["JavaScript", 95], ["TypeScript", 75], ["HTML/CSS", 95], ["Sass/LESS", 75], ["jQeury", 85], ["Angular", 90], ["Angular 2", 85], ["React", 75], ["React Native", 65], ["Backbone", 65], ["Node/Express", 95], ["MongoDB", 95], ["MySQL", 95], ["Git", 95], ["Grunt/Gulp", 65], ["Mocha/Chai", 65]]
 });
