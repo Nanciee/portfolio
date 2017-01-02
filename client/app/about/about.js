8875e28b7517e5780581cd9efb1a08b41f9a57ac
Nanciee <nancyduyi@gmail.com>
2017-01-01 22:10:37 -0500

    Change dash to dot dividers

--- client/app/about/about.js
+++ client/app/about/about.js
@@ -85,8 +85,11 @@ angular.module('personal.about', [])
 
 	$(function(){
       $(".typed").typed({
-          strings: ["^1500<br>^100<br>^100<span class='blue'>const</span> myName <span class='pink'>=</span><span class='yellow'> 'Nancy Du'</span>;^500<br>^200<span class='blue'>const</span> myTitle <span class='pink'>=</span><span class='yellow'> 'Full Stack Web Developer'</span>;^500<br>^200<br>^400<span class='blue'>greet</span>(myName, myTitle);"],
-          typeSpeed: 20
+          strings: ["^1500<br>^100<br>^100<span class='blue'>const</span> myName <span class='pink'>=</span><span class='yellow'> 'Nancy Du'</span>;^500<br>^200<span class='blue'>const</span> myTitle <span class='pink'>=</span><span class='yellow'> 'Full Stack Web Developer'</span>;^500<br>^200<br>^400<span class='blue'>greet</span>(myName, myTitle);^1500"],
+          typeSpeed: 30,
+          callback: function() {
+          	console.log("done")
+          },
       });
   });
 });
